8a1594edc4562cb710dacc1db317792c93a52c9c
Mark Zacharias <mark@punchcard.io>
2022-06-02 12:53:37 -0600

    trial using react-qr-barcode-scanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BarcodeScannerComponent from 'react-qr-barcode-scanner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarcodeScannerComponent width={500} height={500} onUpdate={(one, two) => console.log(one, two)} />
     </div>
   );
 }
